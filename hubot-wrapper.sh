13937fcc4bdfde506b230eff90a86cbaca036770
Stephen Garlick <sgarlick@taximagic.com>
2015-04-20 19:01:03 -0400

    add log level

--- hubot-wrapper.sh
+++ hubot-wrapper.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
 export HUBOT_HIPCHAT_JID=$(curl consul:8500/v1/kv/hubot/HUBOT_HIPCHAT_JID\?raw)
 export HUBOT_HIPCHAT_PASSWORD=$(curl consul:8500/v1/kv/hubot/HUBOT_HIPCHAT_PASSWORD\?raw)
+export HUBOT_LOG_LEVEL=$(curl consul:8500/v1/kv/hubot/HUBOT_LOG_LEVEL\?raw)
 
 exec /opt/hubot/bin/hubot -a hipchat
