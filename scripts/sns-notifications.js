f8d0df2cf8162070616947fb75efbb314cdc44c3
Stephen Garlick <sgarlick@taximagic.com>
2015-04-21 14:12:10 -0400

    handle alL

--- scripts/sns-notifications.js
+++ scripts/sns-notifications.js
@@ -2,10 +2,12 @@ module.exports = function(robot) {
     room = process.env.HUBOT_NOTIFICATION_ROOM;
 
     robot.on("sns:notification", function(msg) {
+      if(msg.topic != "curbformation-stage-notifications") {
         robot.messageRoom(room,
             "Received notification:" +
             "\nTopic: " + msg.topic +
             "\nMessage: " + msg.message);
+      }
     });
 
     parser = require("properties-parser")
@@ -13,7 +15,8 @@ module.exports = function(robot) {
         message = parser.parse(msg.message);
         params = JSON.parse(
             message.ResourceProperties.substring(
-                1, message.ResourceProperties.lenght - 1));
+                1, message.ResourceProperties.length - 1));
+        
         if (message.StackName === message.LogicalResourceId) {
             robot.messageRoom(room,
                 "Topic: " + msg.topic +
