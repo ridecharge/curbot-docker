c528a5d8cc08abd8ebebd8341d079b530c280e75
Stephen Garlick <sgarlick@taximagic.com>
2015-04-21 13:58:07 -0400

    strip single quotes

--- scripts/sns-notifications.js
+++ scripts/sns-notifications.js
@@ -11,13 +11,15 @@ module.exports = function(robot) {
     parser = require("properties-parser")
     robot.on("sns:notification:curbformation-stage-notifications", function(msg) {
         message = parser.parse(msg.message);
-        params = JSON.parse(message.ResourceProperties);
-        if(message.StackName === message.LogicalResourceId) {
-          robot.messageRoom(room,
-              "Topic: " + msg.topic +
-              "\nStack: " + message.StackName +
-              "\nStatus: " + message.ResourceStatus +
-              "\nVersion: " + params.Version);
+        params = JSON.parse(
+            message.ResourceProperties.substring(
+                1, message.ResourceProperties.lenght - 1));
+        if (message.StackName === message.LogicalResourceId) {
+            robot.messageRoom(room,
+                "Topic: " + msg.topic +
+                "\nStack: " + message.StackName +
+                "\nStatus: " + message.ResourceStatus +
+                "\nVersion: " + params.Version);
         }
     });
 }
