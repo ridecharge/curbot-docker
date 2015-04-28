022f1b2f6aed66094e5d54a888e119aaeff79213
Stephen Garlick <sgarlick@taximagic.com>
2015-04-28 13:12:14 -0400

    build

--- Makefile
+++ Makefile
@@ -1,11 +1,14 @@
 DOCKER_REPO=registry.gocurb.internal:80
 CONTAINER=$(DOCKER_REPO)/curbot
 
-all: build push
+all: build push clean
 
 build:
-	docker build -t $(CONTAINER):$(shell git rev-parse HEAD) . 
+	docker build --no-cache -t $(CONTAINER):$(shell git rev-parse HEAD) . 
 	docker build -t $(CONTAINER):latest . 
 
 push:
 	docker push $(CONTAINER)
+
+clean:
+	docker rmi $(CONTAINER)
--- scripts/sns-notifications.js
+++ scripts/sns-notifications.js
@@ -2,25 +2,23 @@ module.exports = function(robot) {
     room = process.env.HUBOT_NOTIFICATION_ROOM;
 
     robot.on("sns:notification", function(msg) {
-      if(msg.topic != "curbformation-stage-notifications") {
-        robot.messageRoom(room,
-            "Received notification:" +
-            "\nTopic: " + msg.topic +
-            "\nMessage: " + msg.message);
-      }
+        if (msg.topic != "curbformation-stage-notifications") {
+            robot.messageRoom(room,
+                "Received notification:" +
+                "\nTopic: " + msg.topic +
+                "\nMessage: " + msg.message);
+        }
     });
 
     parser = require("properties-parser")
     robot.on("sns:notification:curbformation-stage-notifications", function(msg) {
         message = parser.parse(msg.message);
-        console.log(message.ResourceProperties)
-        console.log(message)
 
         if (message.StackName === message.LogicalResourceId) {
             robot.messageRoom(room,
                 "Topic: " + msg.topic +
                 "\nStack: " + message.StackName +
-                "\nStatus: " + message.ResourceStatus);
+                "\nStatus: " + message.ResourceStatusß);
         }
     });
 }
